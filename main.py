e0f4b2042c7405c4ce052e3c13a8a14d76550625
gooosexe <olivershaohuang2006@gmail.com>
2024-12-28 02:25:08 -0500

    added quadtree
    - generates a quadtree correctly for the starting state
    - quadtree doesn't work with negative numbers, need to fix some basic arithmetic (likely just zone checking errors)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import animation, colors, colormaps
 import numpy as np
-from body import Body
+from structures import Body, QuadTree
 
 # Simulation scale:
 # 1 pixel per million km
@@ -10,24 +10,12 @@ from body import Body
 # Test 1: simulation of 500 bodies with the same mass and radius
 TSTEP = 1e3 # 1 million seconds per step
 SIM_SPEED = 1 # 8 steps per frame
-SIM_LEN = 1000
+SIM_LEN = 1
 G = 6.67430e-11
-BODIES = 10 
+BODIES = 100
 MASS = 1e24 # 1 septillion kg, roughly 1/5 of earth
 RADIUS = 1e6 # 1 million meters, roughly 1/6 of earth
 SOFT_PARAM = 1e7 # softening parameter
-
-
-state = np.zeros((BODIES, 4)) # x, y, vx, vy for each body
-bodies = []
-for i in range(BODIES):
-    # set random positions (m) and velocities (m s^-1)
-    x = np.random.randint(-1e9, 1e9)
-    y = np.random.randint(-1e9, 1e9)
-    vx = np.random.randint(-1e3, 1e3)
-    vy = np.random.randint(-1e3, 1e3)
-    state[i] = [x, y, vx, vy]
-    bodies.append(Body(np.array([x, y], dtype=float), np.array([vx, vy], dtype=float), MASS, RADIUS))
 
 def gforce(m1, m2, r):
     # calculate gravitational force between two bodies
@@ -57,25 +45,68 @@ def simulate(bodies, tstep, sim_len):
         simulation.append(np.array([body.pos for body in bodies]))
     return np.array(simulation)
 
+def construct_quadtree(bodies):
+    # construct quadtree
+    boundary = [0, 0, 1e9, 1e9]
+    quadtree = QuadTree(boundary, 1)
+    for body in bodies:
+        quadtree.insert(body)
+    return quadtree
+
+def return_quadtree_lines(quadtree):
+    x, y, width, height = quadtree.boundary
+    lines = []
+    if quadtree.children == []:
+        x, y, width, height = quadtree.boundary
+        lines.append((x, y, x + width, y))
+        lines.append((x, y, x, y + height))
+        lines.append((x + width, y, x + width, y + height))
+        lines.append((x, y + height, x + width, y + height))
+        return lines
+    else:
+        lines.append((x, y, x + width, y))
+        lines.append((x, y, x, y + height))
+        lines.append((x + width, y, x + width, y + height))
+        lines.append((x, y + height, x + width, y + height))
+        for child in quadtree.children:
+            lines.extend(return_quadtree_lines(child))
+        return lines
+
+state = np.zeros((BODIES, 4)) # x, y, vx, vy for each body
+bodies = []
+for i in range(BODIES):
+    # set random positions (m) and velocities (m s^-1)
+    x = np.random.randint(0, 1e9)
+    y = np.random.randint(0, 1e9)
+    vx = np.random.randint(-1e3, 1e3)
+    vy = np.random.randint(-1e3, 1e3)
+    state[i] = [x, y, vx, vy]
+    bodies.append(Body(np.array([x, y], dtype=float), np.array([vx, vy], dtype=float), MASS, RADIUS))
+quadtree = construct_quadtree(bodies)
+
 scale = 1e-6
 bound = 1e9*scale
 simulation = simulate(bodies, TSTEP, SIM_LEN)
+print(f'{quadtree.count()} nodes in quadtree')
 
 fig = plt.figure()
-scatter = plt.scatter([], [], s=1, c='black', vmin=-1e1, vmax=1e1)
+#scatter = plt.scatter([], [], s=1, c='black', vmin=-1e1, vmax=1e1)
+scatter = plt.scatter(simulation[0][:, 0]*scale, simulation[0][:, 1]*scale, s=1, c='black')
+for x1, y1, x2, y2 in return_quadtree_lines(quadtree):
+    plt.plot([x1*scale, x2*scale], [y1*scale, y2*scale], 'r-')
 ax = fig.get_axes()
-ax[0].set_xlim(-bound, bound)
-ax[0].set_ylim(-bound, bound)
-ax[0].set_title('500 bodies equal masses and radii')
+ax[0].set_xlim(0, bound)
+ax[0].set_ylim(0, bound)
+#ax[0].set_title('500 bodies equal masses and radii')
 ax[0].set_xlabel('x (million km)')
 ax[0].set_ylabel('y (million km)')
 
 plt.gca().set_aspect('equal', adjustable='box')
 
-def update(frame):
-    scatter.set_offsets(simulation[frame*SIM_SPEED]*scale)
-    return scatter,
-anim = animation.FuncAnimation(fig, update, frames=range(SIM_LEN//SIM_SPEED), interval=50, blit=True)
+# def update(frame):
+#     scatter.set_offsets(simulation[frame*SIM_SPEED]*scale)
+#     return scatter,
+# anim = animation.FuncAnimation(fig, update, frames=range(SIM_LEN//SIM_SPEED), interval=50, blit=True)
 
 plt.show()
 plt.close()
---
+++ structures.py
@@ -0,0 +1,149 @@
+import numpy as np
+
+class QuadTree:
+    """A class to represent a quadtree.
+    === Instance Attributes ===
+    boundary: The boundary of the quadtree in the form of a list of 4 integers [x, y, width, height]. 
+    capacity: The maximum number of bodies that can be stored in a quadtree node. 
+    bodies: A list of bodies in the quadtree node. 
+    children: A list of 4 quadtree nodes that are children of the current node.
+    """
+    boundary: list
+    capacity: int
+    bodies: list
+    children: list
+
+    def __init__(self, boundary, capacity):
+        self.boundary = boundary
+        self.capacity = capacity
+        self.bodies = []
+        self.children = []
+
+    def insert(self, body):
+        """Inserts a body into the quadtree. 
+        If the number of bodies in the node exceeds the capacity, the node is subdivided into 4 children.
+        """
+        if len(self.children) > 0:
+            index = self.get_index(body)
+            # if the body fits in a child node, insert it into that node
+            if index != -1:
+                self.children[index].insert(body)
+                return
+        self.bodies.append(body)
+        if len(self.bodies) > self.capacity:
+            if len(self.children) == 0:
+                self.subdivide()
+            i = 0
+            while i < len(self.bodies):
+                index = self.get_index(self.bodies[i])
+                if index != -1:
+                    self.children[index].insert(self.bodies.pop(i))
+                else:
+                    i += 1
+
+    def subdivide(self):
+        """Subdivides the quadtree into 4 children.
+        """
+        x, y, width, height = self.boundary
+        # lower right, lower left, upper left, upper right
+        self.children.append(QuadTree([x + width // 2, y, width // 2, height // 2], self.capacity))
+        self.children.append(QuadTree([x, y, width // 2, height // 2], self.capacity))
+        self.children.append(QuadTree([x, y + height // 2, width // 2, height // 2], self.capacity))
+        self.children.append(QuadTree([x + width // 2, y + height // 2, width // 2, height // 2], self.capacity))
+
+    def get_index(self, body):
+        """Returns the index of the child that the body sits in.
+        """
+        x, y, width, height = self.boundary
+        pos = body.pos
+        if x + width // 2 > pos[0] >= x:
+            if y + height // 2 > pos[1] >= y:
+                return 1
+            elif y + height > pos[1] >= y + height // 2:
+                return 2
+        elif x + width > pos[0] >= x + width // 2:
+            if y + height // 2 > pos[1] >= y:
+                return 0
+            elif y + height > pos[1] >= y + height // 2:
+                return 3
+        return -1
+    
+    def query(self, boundary, found):
+        """Returns a list of bodies that are within the boundary.
+        """
+        if self.intersects(boundary):
+            for body in self.bodies:
+                if boundary[0] <= body.pos[0] < boundary[0] + boundary[2] and boundary[1] <= body.pos[1] < boundary[1] + boundary[3]:
+                    found.append(body)
+            for child in self.children:
+                child.query(boundary, found)
+        return found
+    
+    def count(self):
+        """Returns the number of boxes in the quadtree.
+        """
+        if len(self.children) == 0:
+            return 1
+        else:
+            count = 0
+            for child in self.children:
+                count += child.count()
+            return count
+    
+    def contains(self, body):
+        """Returns whether the quadtree node contains the body.
+        """
+        x, y, width, height = self.boundary
+        return x <= body.pos[0] < x + width and y <= body.pos[1] < y + height
+    
+    def intersects(self, boundary):
+        """Returns whether the quadtree node intersects with the boundary.
+        """
+        x, y, width, height = self.boundary
+        return x < boundary[0] + boundary[2] and x + width > boundary[0] and y < boundary[1] + boundary[3] and y + height > boundary[1]
+    
+    def clear(self):
+        """Clears the quadtree.
+        """
+        self.bodies = []
+        self.children = []
+
+
+class Body:
+    """A class to represent a celestial body.
+    === Instance Attributes ===
+    pos: The position of the body in the form of a pygame.Vector2 in meters.
+    vel: The velocity of the body in the form of a pygame.Vector2 in meters
+    per second.
+    acc: The acceleration of the body in the form of a
+    pygame.Vector2 in meters per second squared. mass: The mass of the body
+    in kilograms. radius: The radius of the body in meters.
+    """
+    pos: np.array
+    vel: np.array
+    acc = np.array([0, 0])
+    mass: np.float64
+    radius: np.float64
+
+    def __init__(self, pos, vel, mass, radius):
+        self.pos = pos
+        self.vel = vel
+        self.mass = mass
+        self.radius = radius
+
+    def __str__(self):
+        return (f'pos: {self.pos}, vel: {self.vel}, acc: {self.acc}, '
+                f'mass: {self.mass}, radius: {self.radius}')
+
+    def update(self, delta, timestep):
+        """Updates the position and velocity of the body.
+        """
+        self.vel += self.acc * delta * timestep
+        self.pos += self.vel * delta * timestep
+
+    def render(self):
+        """Renders the body on the screen.
+        Radius is the base 10 log of the mass.
+        Rendered position is the actual position divided by 1 billion.
+        Place 0, 0 at the center of the screen.
+        """
